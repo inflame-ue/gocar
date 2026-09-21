7cd3bd25d06a9c7fa83aa47a5af3c7109d06ba11
Ilia Oberemok <i.oberemok777@gmail.com>
2026-09-21 12:14:34 +0300

    Remove the sentinel error from parser.go that was masking the error messages from the library; update the Tasks type to be a map instead of a slice.

--- internal/parser/parser.go
+++ internal/parser/parser.go
@@ -1,21 +1,17 @@
 package parser
 
 import (
-	"errors"
-
 	"github.com/inflame-ue/gocar/internal/task"
 	"go.yaml.in/yaml/v4"
 )
 
-type Tasks []task.Task
+type Tasks map[string]task.Task
 
-var RuntimeUnmarhsalError = errors.New("parsing of the YAML file with task specification failed")
-
-func ParseTaskSpecification(data []byte) (Tasks, error) {
+func Parse(data []byte) (Tasks, error) {
 	var tasks Tasks
 
 	if err := yaml.Unmarshal(data, &tasks); err != nil {
-		return nil, RuntimeUnmarhsalError
+		return nil, err
 	}
 
 	return tasks, nil
--- internal/parser/parser_test.go
+++ internal/parser/parser_test.go
@@ -1,3 +1,23 @@
 package parser
 
+import (
+	"os"
+	"path/filepath"
+	"testing"
+)
 
+func TestParseTaskSpecification(t *testing.T) {
+	filename := filepath.Join("testdata", "smoke.yaml")
+
+	data, err := os.ReadFile(filename)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	tasks, err := Parse(data)
+	if err != nil {
+		t.Error(err)
+	}
+
+	t.Logf("parsed: %v", tasks)
+}
